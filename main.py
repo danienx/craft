7857dd8834267fabaac2bf6a22126911130e3c62
danienx <136675948+danienx@users.noreply.github.com>
2025-08-28 09:43:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import numpy as np
 projects = ["Project A", "Project B", "Project C", "Project D"]
 print("Dự án ngẫu nhiên:", np.random.choice(projects))
 
---02--04--07--10--11--17--18--19--20--22--23--26
+--02--04--07--10--11--17--18--19--20--22--23--26--28
